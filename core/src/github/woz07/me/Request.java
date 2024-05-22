c7a0df13f06b9f23ec507e9321a7346506e9b5f6
woz07 <105782312+woz07@users.noreply.github.com>
2024-05-22 22:58:43 +0100

    ADD: ip.py which gets ip for multiplayer and writes to temp txt file

--- core/src/github/woz07/me/Request.java
+++ core/src/github/woz07/me/Request.java
@@ -2,4 +2,5 @@ package github.woz07.me;
 
 public class Request {
     public String text;
+    // I should pass through the x + y coordinates of the player when making a request and when making a response
 }
--- scripts/ip.py
+++ scripts/ip.py
@@ -1,8 +1,9 @@
+import socket
+import os
+
 # gets ip of internet service provider
 
-import socket
-
-ip = ""
+ip = ''
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 try:
     s.connect(('8.8.8.8', 80)) # dummy address
@@ -11,4 +12,14 @@ except Exception:
     ip = '127.0.0.1'
 
 # write data to file so java can read
+#
+path = '/data/temp.txt'
+# if os.path.exists(path):
+#     print(f"Path exists: {path}")
 
+try:
+    with open(path, 'w') as file:
+        file.write(ip)
+    print(f"Data successfully written to {path}")
+except Exception as e:
+    print(f"An error occurred while writing to file : {e}")

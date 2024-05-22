6f0379ec3b52b9897c45081e7592214b380612f6
woz07 <105782312+woz07@users.noreply.github.com>
2024-05-22 23:01:52 +0100

    ADD: temp.txt and ip.py finish

--- scripts/ip.py
+++ scripts/ip.py
@@ -1,25 +1,21 @@
 import socket
 import os
 
-# gets ip of internet service provider
-
-ip = ''
+# gets IP of internet service provider
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 try:
-    s.connect(('8.8.8.8', 80)) # dummy address
+    s.connect(('8.8.8.8', 80))  # Dummy address
     ip = s.getsockname()[0]
 except Exception:
     ip = '127.0.0.1'
+finally:
+    s.close()
 
-# write data to file so java can read
-#
-path = '/data/temp.txt'
-# if os.path.exists(path):
-#     print(f"Path exists: {path}")
+# write data to temp txt file in data folder so Java can read
+path = 'data/temp.txt'
 
-try:
-    with open(path, 'w') as file:
-        file.write(ip)
-    print(f"Data successfully written to {path}")
-except Exception as e:
-    print(f"An error occurred while writing to file : {e}")
+# make sure directory exists
+os.makedirs(os.path.dirname(path), exist_ok=True)
+
+with open(path, 'w') as file:
+    file.write(ip)

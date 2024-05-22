47aa5805998367ae458458adcacda15a299cac48
woz07 <105782312+woz07@users.noreply.github.com>
2024-05-22 13:30:01 +0100

    CHANGE: DesktopLauncher.java -> Launcher.java

--- desktop/src/github/woz07/me/Launcher.java
+++ desktop/src/github/woz07/me/Launcher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import github.woz07.me.Game;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
-public class DesktopLauncher {
+public class Launcher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);

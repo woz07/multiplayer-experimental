4c18483ba16e9d3ebf6a9b17129a16c4f9fdfe44
woz07 <105782312+woz07@users.noreply.github.com>
2024-05-23 00:04:53 +0100

    ADD: Launcher::read() which reads data from data/temp.txt and returns it

--- desktop/src/github/woz07/me/Launcher.java
+++ desktop/src/github/woz07/me/Launcher.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import github.woz07.me.Game;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
@@ -35,6 +37,22 @@ public class Launcher {
 			e.printStackTrace();
 			
 			return -1;
+		}
+	}
+	
+	/**
+	 * Method to read data from data/temp.txt
+	 * @return The data read from temp.txt
+	 */
+	public static String read() {
+		try (BufferedReader reader = new BufferedReader((new FileReader("data/temp.txt")))) {
+			// As file only contains 1 line data there's no need for while loop to loop through entire file
+			return reader.readLine(); // Could be null
+		} catch (IOException e) {
+			System.err.println("Failure to read data/temp.txt file as path is incorrect");
+			e.printStackTrace();
+			
+			return null;
 		}
 	}
 }
